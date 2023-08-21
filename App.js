d5d86c2eeac6e6585aaeb38915caf5ab7662397e
sssssqew <sunrise.in.my.dream@gmail.com>
2023-08-21 16:09:07 +0900

    리액트 네이티브 CLI 설치

---
+++ App.js
@@ -0,0 +1,39 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+
+function App(){
+  return (
+    <SafeAreaView style={styles.container}>
+      <View>
+        <Text style={styles.title}>리액트 네이티브 준비완료 !</Text>
+      </View>
+    </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 20,
+    color: 'orange'
+  }
+})
+
+export default App;
