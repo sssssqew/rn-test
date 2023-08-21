4ad5bd70798b6e1dafeef463260e8dd85bb4dbaf
sssssqew <sunrise.in.my.dream@gmail.com>
2023-08-21 17:59:35 +0900

    할일 추가버튼 구현하기

--- components/TodoInsert.js
+++ components/TodoInsert.js
@@ -1,8 +1,19 @@
 import React, { useState } from 'react'
-import { View, TextInput, StyleSheet } from 'react-native'
+import { 
+    View, 
+    Text,
+    TextInput, 
+    TouchableOpacity, 
+    StyleSheet, 
+    Keyboard
+} from 'react-native'
 
 function TodoInsert(){
   const [todoText, setTodoText] = useState('')
+  const onPress = () => {
+    setTodoText('') // 입력창 초기화
+    Keyboard.dismiss() // 키보드 감추기
+  }
   console.log(todoText)
   return (
     <View style={styles.container}> 
@@ -12,7 +23,17 @@ function TodoInsert(){
         selectionColor={'#d6e3ffff'}  // 커서색상
         style={styles.input}
         value={todoText}
-        onChangeText={setTodoText}/>
+        onChangeText={setTodoText} // 입력창에 글자를 입력할때
+        onSubmitEditing={onPress} // 엔터키 눌렀을때
+        />
+        <TouchableOpacity 
+            activeOpacity={0.7} // 버튼 클릭시 투명도 변경
+            onPress={onPress}   // 버튼 클릭시 실행
+        > 
+            <View style={styles.button}>
+                <Text style={styles.buttonText}>추가</Text>
+            </View>
+        </TouchableOpacity>
     </View>
   )
 }
@@ -20,15 +41,33 @@ function TodoInsert(){
 const styles = StyleSheet.create({
   container: {
     height: 50,
-    paddingLeft: 20,
+    paddingLeft: 10,
     borderColor: 'transparent',
     borderTopWidth: 1,
-    justifyContent: 'center',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between'
   },
   input: {
     color: '#a8c8ffff',
     fontSize: 20,
     paddingVertical: 10,
+  },
+  button: {
+    backgroundColor: '#a8c8ffff',
+    width: 80,
+    height: 35,
+    borderRadius: 20,
+    marginRight: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    overflow: 'hidden'
+  },
+  buttonText: {
+    color: '#fff',
+    letterSpacing: 3,
+    fontWeight: 'bold',
+    fontSize: 15
   }
 })
 
