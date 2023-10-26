b596fdbac62f26e1dbbc7634c16b7e7ef753e77d
sssssqew <sunrise.in.my.dream@gmail.com>
2023-10-26 10:51:43 +0900

    랜딩페이지 - 스크롤 인디케이터 추가

--- screens/LandingScreen.js
+++ screens/LandingScreen.js
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { SafeAreaView, View, Text, StyleSheet, StatusBar, ScrollView, Dimensions, ImageBackground  } from 'react-native'
 
 function LandingScreen({navigation}){
     const { width, height } = Dimensions.get('window')
+    const [currentPageIndex, setCurrentPageIndex] = useState(0)
+    console.log('페이지 번호: ', currentPageIndex)
+
+    const setCurrentPage = (e) => {
+        const { x } = e.nativeEvent.contentOffset // x : 스크롤 위치
+        console.log("스크롤 위치: ", x, "화면너비: ", width)
+        const nextPageIndex = Math.ceil(x / width) // x / width : 스크롤 위치 / 화면너비 -> 페이지번호
+        console.log(nextPageIndex)
+        if(nextPageIndex !== currentPageIndex){
+            setCurrentPageIndex(nextPageIndex)
+        }
+    }
     return (
         <>
             <StatusBar hidden></StatusBar>
@@ -12,6 +24,8 @@ function LandingScreen({navigation}){
                     horizontal={true} // 수평 스크롤링
                     scrollEventThrottle={16} // 스크롤 이벤트 감지하는 시간간격 (ms)
                     pagingEnabled={true} // 스크롤시 페이지네이션
+                    showsHorizontalScrollIndicator={false} // 스크롤바 숨기기
+                    onScroll={setCurrentPage}
                 >
                     <View style={{ width, height }}>
                         <ImageBackground source={require('../assets/imgs/landing-1.jpg')} style={{ width, height }}>
@@ -38,6 +52,12 @@ function LandingScreen({navigation}){
                         </ImageBackground>
                     </View>
                 </ScrollView>
+
+                <View style={styles.scrollIndicatorWrapper}>
+                    {Array(3).fill(0).map((_, index) => (
+                        <View key={index} style={[styles.scrollIndicator, { opacity: currentPageIndex === index ? 1: 0.3}]}></View>
+                    ))}
+                </View>
             </SafeAreaView>
         </>
     )
@@ -63,6 +83,21 @@ const styles = StyleSheet.create({
     marginTop: 10,
     color: '#fff',
     textAlign: 'center'
+  },
+  scrollIndicatorWrapper:{
+    position: 'absolute',
+    left: 0, right: 0,
+    bottom: 50,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  scrollIndicator: {
+    height: 10,
+    width: 10,
+    borderRadius: 10 / 2,
+    backgroundColor: '#aaa',
+    marginLeft: 10,
   }
 })
 export default LandingScreen
