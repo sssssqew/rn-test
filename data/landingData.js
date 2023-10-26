dc4ce32e5a61458011587d7014cfc0cb22f20aaf
sssssqew <sunrise.in.my.dream@gmail.com>
2023-10-26 11:37:18 +0900

    랜딩페이지 보여주기 - 코드 리팩토링

---
+++ data/landingData.js
@@ -0,0 +1,21 @@
+import landingImg1 from '../assets/imgs/landing-1.jpg'
+import landingImg2 from '../assets/imgs/landing-2.jpg'
+import landingImg3 from '../assets/imgs/landing-3.jpg'
+
+export default landingData = [
+    {
+        title: '시간관리',
+        description: '오늘 하루 시간이 너무 빨리 지나갔나요?',
+        source: landingImg1
+    },
+    {
+        title: '히스토리 관리',
+        description: '과거에 무엇을 했는지 기억하시나요?',
+        source: landingImg2
+    },
+    {
+        title: 'SunTODO',
+        description: '그럼 오늘부터 SunTODO 앱을 사용해보시는건 어떨까요?',
+        source: landingImg3
+    }
+]
--- screens/LandingScreen.js
+++ screens/LandingScreen.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
 import { SafeAreaView, View, Text, StyleSheet, StatusBar, ScrollView, Dimensions, ImageBackground  } from 'react-native'
+import LandingPage from '../components/LandingPage'
+import landingData from '../data/landingData'
 
 function LandingScreen({navigation}){
     const { width, height } = Dimensions.get('window')
@@ -27,30 +29,13 @@ function LandingScreen({navigation}){
                     showsHorizontalScrollIndicator={false} // 스크롤바 숨기기
                     onScroll={setCurrentPage}
                 >
-                    <View style={{ width, height }}>
-                        <ImageBackground source={require('../assets/imgs/landing-1.jpg')} style={{ width, height }}>
-                            <View style={[styles.textContent, { width, height }]}>
-                                <Text style={styles.title}>시간관리</Text>
-                                <Text style={styles.description}>오늘 하루 시간이 너무 빨리 지나갔나요?</Text>
-                            </View>
-                        </ImageBackground >
-                    </View>
-                    <View style={{ width, height }}>
-                        <ImageBackground source={require('../assets/imgs/landing-2.jpg')} style={{ width, height }}>
-                            <View style={[styles.textContent, { width, height }]}>
-                                <Text style={styles.title}>히스토리 관리</Text>
-                                <Text style={styles.description}>과거에 무엇을 했는지 기억하시나요?</Text>
-                            </View>
-                        </ImageBackground>
-                    </View>
-                    <View style={{ width, height }}>
-                        <ImageBackground source={require('../assets/imgs/landing-3.jpg')} style={{ width, height }}>
-                            <View style={[styles.textContent, { width, height }]}>
-                                <Text style={styles.title}>SunTODO</Text>
-                                <Text style={styles.description}>그럼 오늘부터 SunTODO 앱을 사용해보시는건 어떨까요?</Text>
-                            </View>
-                        </ImageBackground>
-                    </View>
+                    {landingData.map(page => (
+                        <LandingPage
+                            width={width}
+                            height={height}
+                            {...page}
+                        />
+                    ))}
                 </ScrollView>
 
                 <View style={styles.scrollIndicatorWrapper}>
@@ -66,23 +51,6 @@ function LandingScreen({navigation}){
 const styles = StyleSheet.create({
   block: {
     flex: 1
-  },
-  textContent: {
-    justifyContent: 'flex-start',
-    alignItems: 'center',
-    padding: 85,
-  },
-  title: {
-    fontSize: 35,
-    fontWeight: 'bold',
-    color: '#fff'
-  },
-  description: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    marginTop: 10,
-    color: '#fff',
-    textAlign: 'center'
   },
   scrollIndicatorWrapper:{
     position: 'absolute',
